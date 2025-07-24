a638975432b8356f52404bf87c66bf7f33c65861
abhibisht1005 <abhishekbisht1407@gmail.com>
2025-07-24 19:32:13 +0530

    Update FirebaseConfig.jsx

--- vite-project/src/firebase/FirebaseConfig.jsx
+++ vite-project/src/firebase/FirebaseConfig.jsx
@@ -6,13 +6,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyCAu7f8CiR89bD59zADRjRidlBEMwAORZg",
+//use your own firbase apikey and info
+  apiKey: "AIzaSyCAu7f8CiR89bD59zADRjRidlMw",
   authDomain: "myecom-5de3d.firebaseapp.com",
-  projectId: "myecom-5de3d",
+  projectId: "myecom-5de3",
   storageBucket: "myecom-5de3d.firebasestorage.app",
   messagingSenderId: "20246265190",
-  appId: "1:20246265190:web:c5203180fd0bc0a36d1ca7"
+  appId: "1:20246265190:web:c5203180fd0bd1ca7"
 };
 
 
